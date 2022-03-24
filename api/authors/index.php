97ce0e6b3c442cdcb7155475927b43f7b5318878
Bryce Thornton <89411777+brthornton2@users.noreply.github.com>
2022-03-23 23:04:07 -0500

    Update index.php

--- api/authors/index.php
+++ api/authors/index.php
@@ -12,7 +12,43 @@
     elseif ($method === 'GET' && isset($_GET['id'])){
       require('read_single.php');
     elseif ($method === 'GET'){
-      require('read.php');
+      header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json');
+
+        include_once '../../config/database.php';
+        include_once '../../models/authors.php';
+
+        $database = new database();
+        $db = $database->connect();
+
+        $author = new authors($db);
+
+        $result = $author->read();
+
+        $num = $result->rowCount();
+
+        if($num > 0){
+            $author_arr = array();
+            $author_arr['data'] = array();
+
+            while($row = $result->fetch(PDO::FETCH_ASSOC)){
+                extract($row);
+
+                $author_item = array(
+                    'id' => $id,
+                    'author' => $author
+                );
+                array_push($author_arr['data'], $author_item);
+            }
+
+            echo json_encode($author_arr);
+
+        }
+        else{
+            echo json_encode(
+                array('message' => 'No authors found')
+            );
+        }
     elseif ($method === 'DELETE'){
       require('delete.php');
     elseif ($method === 'POST'){
