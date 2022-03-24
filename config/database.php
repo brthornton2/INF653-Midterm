c600b9e25d5c48d550371c7129bf0a0f43e34690
Bryce Thornton <89411777+brthornton2@users.noreply.github.com>
2022-03-23 21:21:34 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
         private $host = 'uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $db_name = 'l3slwtpfycyq0hnk';
         private $username = 'xsf7f9vyxm12i5tq';
-        private $password = 'p01xfa4oaeww8cba';
+        private $password = getenv('p01xfa4oaeww8cba');
         private $conn;
 
         public function connect(){
