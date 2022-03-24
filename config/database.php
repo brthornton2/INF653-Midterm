4f25f9393fefdf886a52c318a45b62b9e88e893b
Bryce Thornton <89411777+brthornton2@users.noreply.github.com>
2022-03-23 21:03:03 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
     class database {
         private $host = 'uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-        private $db_name = '	l3slwtpfycyq0hnk';
+        private $db_name = 'l3slwtpfycyq0hnk';
         private $username = 'xsf7f9vyxm12i5tq';
         private $password = 'p01xfa4oaeww8cba';
         private $conn;
