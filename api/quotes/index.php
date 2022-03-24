9f08f0dad731dd6477396dcbf2fc4f8cd2960257
Bryce Thornton <89411777+brthornton2@users.noreply.github.com>
2022-03-23 23:51:43 -0500

    Update index.php

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -11,7 +11,7 @@
     }
     if ($method === 'GET' && isset($_GET['id'])){
       require('read_single.php');
-      die;
+      die();
     }
     if ($method === 'GET'){
       require('read.php');
